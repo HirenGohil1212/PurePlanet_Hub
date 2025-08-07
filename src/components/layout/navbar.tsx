9e19592c24a4e05ef3e81eaf6c599d0cf2d123f8
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-08-07 03:16:59 +0000

    see here my logo go out of navbar fix it now

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -77,8 +77,8 @@ export function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-32 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <Link href="/" className="flex items-center" style={{ height: '131%' }}>
+      <div className="container mx-auto flex h-48 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        <Link href="/" className="flex h-full items-center">
           <Image src="/logo1.png?v=2" alt="Pure Planet Recycling Logo" width={256} height={64} className="h-full w-auto" />
         </Link>
 
