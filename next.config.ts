32619fb2c2b980c817477d327f83a44cf869fe29
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-08-19 12:53:46 +0000

    content in out folder

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  trailingSlash: true,
+  trailingSlash: false,
   typescript: {
     ignoreBuildErrors: true,
   },
