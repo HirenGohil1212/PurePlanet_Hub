ea5772f65f6b2c535a50b64602cca553604700b7
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-08-19 11:24:50 +0000

    just remove pioneering word from it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,9 +177,9 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
                 <div className="space-y-2">
                     <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">About Us</div>
-                    <h2 className="font-headline text-3xl font-bold tracking-tighter sm:text-5xl fade-in-up">Pioneering Sustainable Solutions</h2>
+                    <h2 className="font-headline text-3xl font-bold tracking-tighter sm:text-5xl fade-in-up">Sustainable Solutions</h2>
                     <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                        Discover our mission to pioneer sustainable solutions for a cleaner planet. We are a team of passionate individuals committed to responsible e-waste management and environmental stewardship through innovative recycling programs.
+                        Discover our mission to provide sustainable solutions for a cleaner planet. We are a team of passionate individuals committed to responsible e-waste management and environmental stewardship through innovative recycling programs.
                     </p>
                 </div>
             </div>
