f2eec8f32a656f03e2cd1b1da63900b4746e6d0e
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-08-19 12:52:24 +0000

    in short if i go to any page and refresh it that page has to be refrsh a

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  trailingSlash: false,
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
